d4232d698d21e677fda6600ef1b735c7e6ef94d5
harousaidi8 <harounsaidi8@gmail.com>
2023-04-02 15:22:52 +0100

    message :feature 1

--- app/src/main/java/com/wstechnologies/eduardocicd/MainActivity.kt
+++ app/src/main/java/com/wstechnologies/eduardocicd/MainActivity.kt
@@ -2,10 +2,23 @@ package com.wstechnologies.eduardocicd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edittext = findViewById<EditText>(R.id.edit_name)
+        val button = findViewById<Button>(R.id.button)
+        val txt_name = findViewById<TextView>(R.id.txt_name)
+        button.setOnClickListener {
+            txt_name.text=edittext.text.toString()
+        }
+
     }
 }
